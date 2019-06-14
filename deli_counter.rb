8b01f2c8c881aa81ae98e39cc5d94e5332aecf45
Phyllis Kan <pyk9q7@gmail.com>
2019-06-14 21:44:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(array)
   end
 end
 
-def take_a_number (array, name)
+def take_a_number(array, name)
    array.push(name)
    puts "Welcome, #{name}. You are number #{array.length} in line."
 end
